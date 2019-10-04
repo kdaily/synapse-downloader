830a245752562fcec968818a331240ca3ab590ae
kdaily <kenneth.daily@sagebase.org>
2019-10-04 13:46:07 -0700

    make proj, folder, and files for each method

--- download_test.sh
+++ download_test.sh
@@ -7,33 +7,27 @@ NDIR=5 # number of directories to create $NFILES in
 MB=25 # MB file size of each file
 CACHE_DIR=/home/kdaily/.synapseCache
 
-PROJID=$(synapse create --name 'Test Synapse Downloader' Project | grep "Create" | sed "s/.*\(syn[0-9]*\).*/\1/");
-
-for FOLDERNUM in $(seq 1 ${NDIR}) ; do
-    FOLDERNAME="Folder ${FOLDERNUM}";
-    FOLDERID=$(synapse create --name "${FOLDERNAME}" Folder --parentId ${PROJID} | grep "Create" | sed "s/.*\(syn[0-9]*\).*/\1/");
-
-    echo "#####################" ;
-    echo "Creating files" ;
-    echo "#####################" ;
-
-    seq 1 ${NFILES} | xargs -I {} -n 1 dd status=none if=/dev/urandom of=/tmp/synapse-downloader/test-files/file{}.txt bs=1048576 count=${MB} ;
-
-    echo "#####################" ;
-    echo "Storing to Synapse" ;
-    echo "#####################" ;
-
-    seq 1 ${NFILES} | xargs -I {} -n 1 -P 4 synapse store --parentId ${FOLDERID} /tmp/synapse-downloader/test-files/file{}.txt > /dev/null 2>&1 ;
-
-    rm /tmp/synapse-downloader/test-files/file*.txt ;
-done
-
-
 for METHOD in new old sync ; do
-
     echo "#####################" ;
     echo "Using ${METHOD}" ;
     echo "#####################";
+
+    echo "#####################" ;
+    echo "Setup project, folders, and files." ;
+    echo "#####################" ;
+
+    PROJID=$(synapse create --name 'Test Synapse Downloader' Project | grep "Create" | sed "s/.*\(syn[0-9]*\).*/\1/");
+
+    for FOLDERNUM in $(seq 1 ${NDIR}) ; do
+        FOLDERNAME="Folder ${FOLDERNUM}";
+        FOLDERID=$(synapse create --name "${FOLDERNAME}" Folder --parentId ${PROJID} | grep "Create" | sed "s/.*\(syn[0-9]*\).*/\1/");
+
+        seq 1 ${NFILES} | xargs -I {} -n 1 dd status=none if=/dev/urandom of=/tmp/synapse-downloader/test-files/file{}.txt bs=1048576 count=${MB} ;
+
+        seq 1 ${NFILES} | xargs -I {} -n 1 -P 4 synapse store --parentId ${FOLDERID} /tmp/synapse-downloader/test-files/file{}.txt > /dev/null 2>&1 ;
+
+        rm /tmp/synapse-downloader/test-files/file*.txt ;
+    done
 
     echo "#####################" ;
     echo "Clear cache" ;
